6687c14105739602cda97ebe3d1fe77cb9b82917
SaluteBEE <salutebee@outlook.com>
2022-11-16 20:34:35 +0800

    升级播报时会对不同等级生成不同的背景颜色

--- Assets/Scripts/UI/Announcement.cs
+++ Assets/Scripts/UI/Announcement.cs
@@ -1,27 +1,31 @@
 using UnityEngine;
 using System.Collections;
 using TMPro;
+using UnityEngine.UI;
 
 public class Announcement : MonoBehaviour
 {
     public TextMeshProUGUI annText;
     public float showTime = 5.0f;
-
+    public GameObject announcer;
+    public Color[] announcerColors;
+    public Image annoucerImage;
     private void Start()
     {
-        annText.gameObject.SetActive(false);
+        announcer.gameObject.SetActive(false);
     }
 
     public void LevelUpAnn(Player player)
     {
+        annoucerImage.color = announcerColors[player.level];
         annText.text = player.playerName + ConstantText.levelUpAnn + player.level;
         StartCoroutine(AnnTextDisappearCoroutine());
     }
 
     IEnumerator AnnTextDisappearCoroutine()
     {
-        annText.gameObject.SetActive(true);
+        announcer.gameObject.SetActive(true);
         yield return new WaitForSeconds(showTime);
-        annText.gameObject.SetActive(false);
+        announcer.gameObject.SetActive(false);
     }
 }
